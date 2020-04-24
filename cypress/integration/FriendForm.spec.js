49bd58cb4f735519a0c0ace2a84c8f8c269da2e1
NomadDaniel <reachingdaniel@gmail.com>
2020-04-23 22:09:43 -0400

    added validation

--- cypress/integration/FriendForm.spec.js
+++ cypress/integration/FriendForm.spec.js
@@ -4,19 +4,26 @@ describe('Testing user onboarding form', function () {
     cy.visit("http://localhost:3000")
   })
   it("Add test to input and submit form", function () {
+    
     cy.get('input[name="firstName"]')
       .type("Dan")
-      .should("have.value", "Dan")
+      .should("have.value", "Dan")    
     cy.get('input[name="lastName"]')
       .type("Anderson")
-      .should("have.value", "Anderson")
+      .should("have.value", "Anderson")  
     cy.get('input[name="email"]')
       .type("dan@dan.com")
       .should("have.value", "dan@dan.com")
     cy.get('[type="checkbox"]').check()
-      .check()
       .should("be.checked")
-    cy.get('button').click()
-                   
+    cy.get('button').click()  
   })
+  
+  it("Validates that validation errors are thrown", function () {
+
+    cy.get('input[name="firstName"]').type('Dan').clear()
+    cy.get('input[name="lastName"]').type('Dan').clear()
+    cy.get('input[name="email"]').type('dan@dan.com').clear()
+  })
+
 })
